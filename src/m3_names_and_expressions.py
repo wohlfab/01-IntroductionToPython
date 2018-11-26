b781990c5f768af8bfea0097329cf23ec6612467
wohlfab <wohlfab@rose-hulman.edu>
2018-11-26 14:25:05 -0500

    Classwork 1

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -105,7 +105,7 @@ print(type(math))
 print(type('math'))
 ###############################################################################
 #
-# DONE : 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
